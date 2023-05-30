63b2cb72dcf59136c07a0fa6f2473dfa78484f4e
Abdijibar123 <abdijibar1313@gmail.com>
2023-05-30 19:19:52 +0300

    done with football exercise

--- challenge.js
+++ challenge.js
@@ -1,2 +1,13 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+let win=3
+let draw=1
+let loss=0
+
+function footballPoints(wins, draws, losses) {
+    return win * wins + draw * draws + loss * losses
+}
+
+
+
+console.log( footballPoints(3, 4, 2)) 
+console.log(footballPoints(5, 0, 2)) 
+console.log(footballPoints(5, 8, 1)) 
